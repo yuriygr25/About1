4ff7524d4a727a8ec3cc6e2203f66a94c2053f5a
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-23 21:29:48 +0300

    commit

---
+++ app/src/main/java/com/example/yura/about/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.example.yura.about;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/example/yura/about/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.yura.about;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+}
